255b52206b23881bf45c65103c1026c4e68744b3
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2014-11-25 13:46:34 -0500

    Switching to npm+browserify from bower

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
 'use strict';
 
-require.config({
-    baseUrl: document.URL.substring(0, document.URL.lastIndexOf("/")) + '/js/',
-    paths: {
-        'superagent': 'libs/superagent/superagent',
-        'superagent-valence': 'libs/superagent-valence/index',
-    }
-});
+var request = require('superagent');
+var valence = require('superagent-valence')('MySampleApp');
 
-require(['superagent', 'superagent-valence'], function(request, valence) {
-    var valence = valence('MySampleApp');
-
-    request
-    .get('/d2l/api/lp/1.4/users/whoami')
-    .use(valence)
-    .end(function(res) {
-        var user = res.body;
-        var name = document.getElementById('name');
-        name.textContent = 'Hello, ' + user.FirstName + ' ' + user.LastName;
-    });
-});
+request
+	.get('/d2l/api/lp/1.4/users/whoami')
+	.use(valence)
+	.end(function(res) {
+		var user = res.body;
+		var name = document.getElementById('name');
+		name.textContent = 'Hello, ' + user.FirstName + ' ' + user.LastName;
+	});
