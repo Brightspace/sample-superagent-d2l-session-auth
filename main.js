52cf19469f5ab4cf9de11e945da792f2f239210e
Owen Smith <owen@omsmith.ca>
2016-08-15 15:17:54 -0400

    fixup sample
    - use published auth package on npm
    - fix versioning conflict
    - use latest auth package
    - add error checks
    
    Fixes https://github.com/Brightspace/sample-superagent-d2l-session-auth/issues/3
    Fixes https://github.com/Brightspace/sample-superagent-d2l-session-auth/issues/4

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 'use strict';
 
 var request = require('superagent');
-var valence = require('superagent-d2l-session-auth')('MySampleApp');
+var valence = require('superagent-d2l-session-auth')();
 
 request
 	.get('/d2l/api/lp/1.4/users/whoami')
 	.use(valence)
-	.end(function(res) {
+	.end(function(err, res) {
+		if (err) {
+			console.log('An error occurred during the whoami request', err);
+
+			var notice = document.createElement('span');
+			notice.textContent = 'An error occurred. Check your browser console for details';
+			document.body.appendChild(notice);
+
+			return;
+		}
+
 		var user = res.body;
 		var name = document.getElementById('name');
 		name.textContent = 'Hello, ' + user.FirstName + ' ' + user.LastName;
